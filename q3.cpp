80473f41061dcddb2edfdc4eb68287e3d28f743b
apratimchoudhury <42172482+apratimchoudhury@users.noreply.github.com>
2018-09-06 14:31:20 +0000

    Update q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -5,43 +5,25 @@ using namespace std;
 //write main function
 int main()
 {
+	//declare variables
+	int a;
+	//take input
+	cout<<"Enter the size of pattern to be printed: ";
+	cin>>a;
 	//main loop	
-	for(int i=0;i<5;i++)
+	for(int i=1;i<=a;i++)
 	{
-		//conditional loop 1		
-		if(i==0||i==4)
+		for(int j=1;j<=a;j++)
 		{
-			for(int j=0;j<5;j++)
+			if(i==1||i==a||j==1||j==a||i==j||i==a-j+1)
 			{
 				cout<<"*";
 			}
+			else
+				cout<<" ";
 		}
-		
-		//conditional loop 2
-		else if(i==1||i==3)
-		{
-			for(int k=0;k<2;k++)
-			{
-				cout<<"*";
-			}
-			cout<<" ";
-			for(int p=0;p<2;p++)
-			{
-				cout<<"*";
-			}
-		}
-		//conditional loop 3		
-		else 
-			for(int l=0;l<5;l++)
-			{
-				if(l==0||l==2||l==4)
-				{
-					cout<<"*";
-				}		
-				else
-					cout<<" ";		
-			}
-		cout<<endl;
+	cout<<endl;	
 	}
+		
 	return 0;
 }
